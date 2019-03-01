18e5a88dde54f8c4e55a225ba339daec793baf66
lichenysq <1454716934@qq.com>
2019-03-01 10:15:13 +0800

    test

--- autoTools/checkMessage3.py
+++ autoTools/checkMessage3.py
@@ -5,6 +5,8 @@ def checkMg3(filePath):
     sfnIndex = None
     reCellIdIndex = None
     reEnbIdIndex = None
+    incellIndex = None
+    enbidIndex = None
     filedatalist = []
 
     with open(filePath) as csvfile:
@@ -13,19 +15,33 @@ def checkMg3(filePath):
             filedatalist.append(row)
 
     for i, item in enumerate(filedatalist[0]):
+
         if item.startswith("MSG_ID"):
             msgIdIndex = i
         elif item.startswith("SFN"):
             sfnIndex = i
-        elif item.startswith("relatedSCellCellId"):
+        elif item.startswith("relatedSCellCellId") or item.startswith("relatedPCellCellId"):
             reCellIdIndex = i
-        elif item.startswith("relatedSCellEnbId"):
+        elif item.startswith("relatedSCellEnbId") or item.startswith("relatedPCellEnbId"):
             reEnbIdIndex = i
+        elif item.startswith("lnCelId"):
+            incellIndex = i
+        elif item.startswith("enbId"):
+            enbidIndex = i
 
     judgeDict = {}
 
+    resultlist = []
     for row in filedatalist:
-        receiver = row[msgIdIndex] + ":" + row[reCellIdIndex] + ":" + row[reEnbIdIndex]
+
+        if row[reCellIdIndex].startswith("relatedSCellCellId"):
+            key1index = reCellIdIndex
+            key2index = reEnbIdIndex
+        else:
+            key1index = incellIndex
+            key2index = enbidIndex
+
+        receiver = row[msgIdIndex] + ":" + row[key1index] + ":" + row[key2index]
         # newsfn16 = row[sfnIndex]
         newsfn = int(row[sfnIndex].replace("SFN=", ""), 16)
 
@@ -37,16 +53,20 @@ def checkMg3(filePath):
                     judgeDict[receiver] = [newsfn, row[0]]
                     continue
                 else:
-                    raise Exception("***** check message3 failed ****** \n"
-                                    "please check:" + row[0] + " and " + judgeDict[receiver][1])
+                    resultlist.append(row[0] + " and " + judgeDict[receiver][1])
+                    continue
             elif newsfn + 1024 - lastsfn <= 4:
                 judgeDict[receiver] = [newsfn, row[0]]
                 continue
             else:
-                raise Exception("***** check message3 failed ****** \n"
-                                "please check:" + row[0] + " and " + judgeDict[receiver][1])
+                resultlist.append(row[0] + " and " + judgeDict[receiver][1])
+                continue
         else:
             judgeDict[receiver] = [newsfn, row[0]]
+
+    if len(resultlist) > 0:
+        raise Exception("***** check message3 failed ****** \n"
+                        "please check:" + str(resultlist))
 
 if __name__ == "__main__":
     # filePath = "C:\\N-5CG8250K26-Data\\shuyu\\Desktop\\enb1_emil.csv"
--- leetcode/test.py
+++ leetcode/test.py
@@ -1,3 +1,19 @@
-L = ['Hello', 'World', 18, 'Apple', None]
-L2 =[s.lower() for s in L if isinstance(s, str)]
-print(L2)
+import random
+print(type(123))
+#
+# def liuhecaijixuan(num):
+#     result = []
+#     for i in range(num):
+#         temp = []
+#         for i in range(5):
+#             tempint = random.randint(1,31)
+#             while tempint in temp:
+#                 tempint = random.randint(1, 31)
+#             temp.append(tempint)
+#         temp.sort()
+#         temp.append(random.randint(1,12))
+#         result.append(temp)
+#     return result
+#
+# for item in liuhecaijixuan(5):
+#     print(item)
